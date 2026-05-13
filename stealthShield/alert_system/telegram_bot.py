dd2129ed80bbd922a60465187c051a144f8360c4
HarishithaSV <harishithasv.cs2023@citchennai.net>
2026-05-13 15:57:04 +0530

    modified telegram_bot.py

--- stealthShield/alert_system/telegram_bot.py
+++ stealthShield/alert_system/telegram_bot.py
@@ -1,31 +1,45 @@
+import os
 import requests
+from dotenv import load_dotenv
 
-# Replace with your actual token and chat_id
-BOT_TOKEN = "8665578250:AAGFgJUFMPE1BeX8TvM4_97ljIDgmEAS1TY"
-CHAT_ID = "1341672672"
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
 
 def send_alert(message):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
     payload = {
         'chat_id': CHAT_ID,
         'text': message
     }
+
     try:
         response = requests.post(url, data=payload)
-        print("📡 Telegram response:", response.text)   # 👈 ADD THIS
+        print("📡 Telegram response:", response.text)
+
     except Exception as e:
         print(f"❌ Failed to send alert: {e}")
 
+
 def send_video(video_path, caption="🎥 Motion Captured"):
-    url = f"https://api.telegram.org/bot8665578250:AAGFgJUFMPE1BeX8TvM4_97ljIDgmEAS1TY/sendVideo"
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendVideo"
+
     try:
         with open(video_path, 'rb') as video:
+
             files = {'video': video}
+
             data = {
                 'chat_id': CHAT_ID,
                 'caption': caption
             }
+
             response = requests.post(url, files=files, data=data)
             response.raise_for_status()
+
     except Exception as e:
         print(f"Failed to send video: {e}")
